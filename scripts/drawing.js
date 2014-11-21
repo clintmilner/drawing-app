81315044a19b21c50964e373c54e8ab228842782
Clint Milner <clint@sympoz.com>
2014-11-21 15:40:47 +0000

    js framework being built

--- scripts/drawing.js
+++ scripts/drawing.js
@@ -1,4 +1,30 @@
-(function()
+var Paintbrush = (function()
 {
+    var ctx = document.getElementById( 'drawing' );
+    return {
+        init: function()
+        {
+            console.log( 'init fired' );
+        },
+        drawClear: function( color )
+        {
 
+        },
+        drawCircle: function( x, y, radius, color )
+        {
+
+        },
+        drawRectangle: function( x, y, w, h, color )
+        {
+
+        },
+        drawLine: function( x1, y1, x2, y2, color )
+        {
+
+        },
+        drawFill: function( x, y, color )
+        {
+
+        }
+    }
 })();
